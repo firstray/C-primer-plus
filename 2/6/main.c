2667027f40a490a1cd58dbf97a4df63d59cd51e2
firstray <522707141@qq.com>
2018-11-13 15:38:37 +0800

    test

---
+++ 2/6/main.c
@@ -0,0 +1,12 @@
+#include"test_1_13.h"
+
+int main(void)
+{
+	int toes = 10;
+	printf("toes:%d\n",toes);
+	printf("double toes %d\n",toes*toes);
+	printf("three toes %d\n",toes*toes*toes);
+	getchar();
+	getchar();
+	return 0;
+}
