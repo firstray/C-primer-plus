7bc674c8d91b0443cf12edaa5eb4061a1b7f38a9
firstray <522707141@qq.com>
2018-11-13 14:50:18 +0800

    main

---
+++ 1/1.13/main.c
@@ -0,0 +1,13 @@
+#include"test_1_13.h"
+
+int main(void)
+{
+	float nums,output;
+
+	scanf_s("%f",&nums);
+	output = MacroMuscle(nums);
+	printf_s("%f",output);
+	getchar();
+	getchar();
+	return 0;
+}
---
+++ 1/1.13/test_1_13.c
@@ -0,0 +1,5 @@
+#include "test_1_13.h"
+float MacroMuscle(float nums){
+	nums = nums*2.54;
+	return nums;
+};
